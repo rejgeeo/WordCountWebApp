b4d2049d547b4e2311d29814bbb55285146b32a8
rejgeeo <rejoyg@gmail.com>
2018-09-29 10:36:54 +0530

    Word Count Application

--- wordcount/wordcount/urls.py
+++ wordcount/wordcount/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('home/', views.home, name="Home"),
+    path('countwords/', views.countwords, name="CountWords"),
+    path('countchars/', views.countchars, name="CountChars"),
+    path('count/', views.count, name="Count"),
+    path('about/', views.about, name="About"),
 ]
